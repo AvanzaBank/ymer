665d2cbb657f1de518904e84bf81854ef3cc55af
Oskar Wiksten <oskar.wiksten@avanza.se>
2021-01-26 14:48:08 +0100

    TP-590: Backport Instant-converters from gs14 branch
    * Backports `Instant` converters from `gs14` branch back to `master`, to keep the same converters available in both branches.
    * Originally created in https://github.com/AvanzaBank/ymer/pull/23

---
+++ ymer/src/test/java/com/avanza/ymer/support/JavaInstantReadConverterTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2015 Avanza Bank AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.avanza.ymer.support;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JavaInstantReadConverterTest {
+
+    private JavaInstantReadConverter javaInstantReadConverter;
+
+    @Before
+    public void setup() {
+        javaInstantReadConverter = new JavaInstantReadConverter();
+    }
+
+    @Test
+    public void shouldConvertISOInstantValueIntoInstantWithNanoPrecision() {
+        // Given
+        Instant expected =
+                LocalDateTime
+                .of(2020, 7, 29, 15, 43, 56, 863000000)
+                .toInstant(ZoneOffset.UTC);
+
+        // When
+        Instant actual = javaInstantReadConverter.convert("2020-07-29T15:43:56.863Z");
+
+        // Then
+        assertThat(actual.getEpochSecond(), is(expected.getEpochSecond()));
+        assertThat(actual.getNano(), is(expected.getNano()));
+    }
+
+}
