93c5998c2056c5db625a6d6ae40eb1e0b89beb35
Patrik Ivarsson <patrik.ivarsson@avanza.se>
2021-12-16 18:27:42 +0100

    TP-828: Code review fixes

--- ymer/src/main/java/com/avanza/ymer/PersistInstanceIdDefinition.java
+++ ymer/src/main/java/com/avanza/ymer/PersistInstanceIdDefinition.java
@@ -13,21 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*
-o * Copyright 2015 Avanza Bank AB
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.avanza.ymer;
 
 public final class PersistInstanceIdDefinition<T> {
@@ -79,7 +64,7 @@ public final class PersistInstanceIdDefinition<T> {
 
 	/**
 	 * Delay starting recalculation of persisted instance id with the specified delay after startup (in seconds).
-	 * This is done do reduce load on database during initial load of data.
+	 * This is done to reduce load on database during initial load of data.
 	 */
 	public PersistInstanceIdDefinition<T> recalculateWithDelay(int recalculateOnStartupDelayInSeconds) {
 		this.recalculateWithDelay = recalculateOnStartupDelayInSeconds;
