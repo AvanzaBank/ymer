7ca5e87e686331ee5c8197eb6a84b3c67569df5d
Patrik Ivarsson <patrik.ivarsson@avanza.se>
2022-02-01 11:10:43 +0100

    TP-939: Small log adjustments to clarify which loading method is used
    * Replace `Begin loadAllObjects` log with a log that also displays which loading method is used.
    * Add collection being operated on in some calculation job logs

--- ymer/src/main/java/com/avanza/ymer/PersistedInstanceIdCalculationService.java
+++ ymer/src/main/java/com/avanza/ymer/PersistedInstanceIdCalculationService.java
@@ -268,12 +268,13 @@ public class PersistedInstanceIdCalculationService implements PersistedInstanceI
 			boolean indexExists = collection.getIndexes()
 					.anyMatch(isIndexForNumberOfPartitions(numberOfPartitions));
 			if (indexExists) {
-				log.info("Step 3/3\tIndex for field [{}] does not need to be created because it already exists", fieldName);
+				log.info("Step 3/3\tIndex for field [{}] in collection {} does not need to be created because it already exists",
+						fieldName, collectionName);
 			} else {
-				log.info("Step 3/3\tCreating index for field [{}]", fieldName);
+				log.info("Step 3/3\tCreating index for field [{}] in collection {}", fieldName, collectionName);
 				IndexOptions options = new IndexOptions().background(true);
 				collection.createIndex(new Document(fieldName, 1), options);
-				log.info("Step 3/3\tDone creating index");
+				log.info("Step 3/3\tDone creating index for field [{}] in collection {}", fieldName, collectionName);
 			}
 		});
 
