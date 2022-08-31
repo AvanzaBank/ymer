9a8b193be2c80d4f0382348a6e955509309dca15
Patrik Ivarsson <patrik.ivarsson@avanza.se>
2022-08-31 12:00:52 +0200

    TP-1209: Add handling of failure statistics in BulkMirroredObjectWriter

--- ymer/src/main/java/com/avanza/ymer/BulkMirroredObjectWriter.java
+++ ymer/src/main/java/com/avanza/ymer/BulkMirroredObjectWriter.java
@@ -31,6 +31,7 @@ import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.avanza.ymer.PerformedOperationsListener.OperationType;
 import com.gigaspaces.sync.DataSyncOperation;
 import com.gigaspaces.sync.OperationsBatchData;
 import com.mongodb.MongoBulkWriteException;
@@ -45,6 +46,13 @@ final class BulkMirroredObjectWriter {
 	private final DocumentWriteExceptionHandler exceptionHandler;
 	private final MirroredObjectFilterer objectFilterer;
 	private final PerformedOperationsListener operationsListener;
+
+	BulkMirroredObjectWriter(SpaceMirrorContext mirror,
+			DocumentWriteExceptionHandler exceptionHandler,
+			MirroredObjectFilterer mirroredObjectFilterer) {
+		this(mirror, exceptionHandler, mirroredObjectFilterer, (type, delta) -> {
+		});
+	}
 
 	BulkMirroredObjectWriter(SpaceMirrorContext mirror,
 			DocumentWriteExceptionHandler exceptionHandler,
@@ -83,8 +91,9 @@ final class BulkMirroredObjectWriter {
 
 		changesByCollection.forEach((collectionName, bulkChanges) -> {
 			List<MongoBulkChange> remainingChanges = bulkChanges;
+			int attempt = 1;
 			while (!remainingChanges.isEmpty()) {
-				remainingChanges = executeMongoDbBulk(collectionName, metadata, remainingChanges);
+				remainingChanges = executeMongoDbBulk(collectionName, metadata, remainingChanges, attempt++);
 			}
 		});
 	}
@@ -95,8 +104,11 @@ final class BulkMirroredObjectWriter {
 	 * @return list of changes that weren't written and needs to be retried.
 	 *         This happens if a row in a bulkWrite failed and needs to be skipped.
 	 */
-	private List<MongoBulkChange> executeMongoDbBulk(String collectionName, InstanceMetadata metadata, List<MongoBulkChange> changes) {
-		Map<Integer, Integer> bulkChangeIdToChangeMap = new HashMap<>();
+	private List<MongoBulkChange> executeMongoDbBulk(String collectionName,
+			InstanceMetadata metadata,
+			List<MongoBulkChange> changes,
+			int attempt) {
+		final Map<Integer, Integer> bulkChangeIdToChangeMap = new HashMap<>();
 		try {
 			DocumentCollection collection = mirror.getDocumentCollection(collectionName);
 
@@ -113,8 +125,12 @@ final class BulkMirroredObjectWriter {
 						versionedDocument = mirror.toVersionedDocument(change.object, metadata);
 						mirror.getPreWriteProcessing(change.object.getClass()).preWrite(versionedDocument);
 					} catch (Exception e) {
-						mirror.onMirrorException(e, change.operation, change.object);
-						exceptionHandler.handleException(e, "Conversion failed, operation: " + change.operation + ", change: " + change.object);
+						// after the first attempt, this error will already have been logged & handled earlier on
+						if (attempt == 1) {
+							mirror.onMirrorException(e, change.operation, change.object);
+							exceptionHandler.handleException(e, "Conversion failed, operation: " + change.operation + ", change: " + change.object);
+							operationsListener.increment(OperationType.FAILURE, 1);
+						}
 						continue;
 					}
 
@@ -147,29 +163,31 @@ final class BulkMirroredObjectWriter {
 			BulkWriteError writeError = e.getWriteErrors().get(0); // always a single write error as we use an ordered operation
 			MongoBulkChange failedChange = changes.get(writeError.getIndex());
 			mirror.onMirrorException(e, failedChange.operation, failedChange.object);
+			operationsListener.increment(OperationType.FAILURE, 1);
 
 			int failedChangeIndex = bulkChangeIdToChangeMap.get(writeError.getIndex());
 			List<MongoBulkChange> remainingChanges = changes.subList(failedChangeIndex + 1, changes.size());
 
 			if (!remainingChanges.isEmpty()) {
-				logger.error("Bulk write failed on a {} operation in collection {}: \"{}\". Will continue writing remaining {} changes",
-						failedChange.operation, collectionName, writeError.getMessage(), remainingChanges.size());
+				logger.error("Bulk write failed attempt {} on a {} operation in collection {}: \"{}\". Will continue writing remaining {} changes",
+						attempt, failedChange.operation, collectionName, writeError.getMessage(), remainingChanges.size());
 			} else {
-				logger.error("Bulk write failed on a {} operation in collection {}: \"{}\". This was the last entry in the batch",
-						failedChange.operation, collectionName, writeError.getMessage(), e);
+				logger.error("Bulk write failed attempt {} on a {} operation in collection {}: \"{}\". This was the last entry in the batch",
+						attempt, failedChange.operation, collectionName, writeError.getMessage(), e);
 			}
 
 			return remainingChanges;
 		} catch (Exception e) {
 			exceptionHandler.handleException(e, "Operation: Bulk write, changes: " + changes);
+			operationsListener.increment(OperationType.FAILURE, changes.size());
 			return emptyList();
 		}
 	}
 
 	private void addResultToStatistics(BulkWriteResult result) {
-		operationsListener.increment(PerformedOperationsListener.OperationType.INSERT, result.getInsertedCount());
-		operationsListener.increment(PerformedOperationsListener.OperationType.UPDATE, result.getMatchedCount());
-		operationsListener.increment(PerformedOperationsListener.OperationType.DELETE, result.getDeletedCount());
+		operationsListener.increment(OperationType.INSERT, result.getInsertedCount());
+		operationsListener.increment(OperationType.UPDATE, result.getMatchedCount());
+		operationsListener.increment(OperationType.DELETE, result.getDeletedCount());
 	}
 
 	private void checkBulkResultForWarnings(int expectedUpdates, int expectedRemovals, BulkWriteResult result) {
